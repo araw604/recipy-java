2895a1cc95c43186caefdd4c240260007277b20d
a raw <vikrr1986@gmail.com>
2015-07-15 13:24:20 -0700

    done

---
+++ script.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+    $('button').click(function() {
+        $(this).toggleClass("ahem");    
+    });
+     $('img').click(function() {
+        $(this).fadeOut('slow');    
+    });
+     $('.sup').click(function() {
+        $(this).toggleClass("abcd");
+        
+        
+        
+    });
+     
+
+        
+
+});
